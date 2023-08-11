961f7e7adb10f9512fd5ac16d046f8cef5028731
carlosfrontend@hotmail.com <11649745+carlosfrontend@users.noreply.github.com>
2023-08-11 18:14:06 +0000

    feat: solve 03_reverseString challenge

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const arrayOfString = [];
+  for (let i = 0; i < string.length; i++) {
+    arrayOfString.push(string[i]);
+  }
+  let reverseText = arrayOfString.reverse().join("");
+  return reverseText;
 };
-
+reverseString("hello world");
 // Do not edit below this line
 module.exports = reverseString;
