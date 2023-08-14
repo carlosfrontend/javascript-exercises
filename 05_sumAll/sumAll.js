ef9c42ef0e4a34e73ce8de21733d8245aaa1e92e
carlosfrontend@hotmail.com <11649745+carlosfrontend@users.noreply.github.com>
2023-08-14 23:02:01 +0000

    feat: solve 05_sumAll challenge

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let sum = 0;
+  if (min < 0 || max < 0) {
+    return "ERROR";
+  }
+  if (typeof min !== "number" || typeof max !== "number") {
+    return "ERROR";
+  }
+  if (min > max) {
+    for (let i = 1; i <= min; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
