ee1fba3bcbe7e7f13a41ef848c1a1f4267724b01
carlosfrontend@hotmail.com <11649745+carlosfrontend@users.noreply.github.com>
2023-08-14 17:11:31 +0000

    feat: resolve 04_removeFromArray challenge

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...args) {
+  return arr.filter((elem) => !args.includes(elem));
 };
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
