3b2db9d6919c355159714068e6dc8ae436b4539f
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-10-02 00:58:16 +0200

    Feat: Finish fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let fibArr = [1, 1];
+  const index = number - 1;
+
+  if (typeof (number) === 'string') {
+    number = +number;
+  }
+
+  if (Math.sign(number) === - 1) {
+    return 'OOPS';
+  } else {
+
+    for (let i = 2; i < number; i++) {
+      fibArr[i] = fibArr[i - 1] + fibArr[i - 2]
+    }
+    return fibArr[index];
+  }
 
 };
 
