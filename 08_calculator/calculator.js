0b327081882384b29db7284b223c67f9649926c8
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-10-01 17:17:45 +0200

    Feat: Finish 08_calculator challenge

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  
+  return a += b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a -= b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, elem) => {
+    return total + elem;
+  },0 );
+}
+
+const multiply = function(arr) {
+ return arr.reduce((acc, number) => {
+  return acc * number;
+ });
 };
 
-const multiply = function() {
-
+const power = function(number, exp) {
+	return Math.pow(number, exp);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	let total = 1;
+  for(let i = 1; i <= number; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
