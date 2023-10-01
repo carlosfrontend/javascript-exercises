907aaf7b9d6fa7d57678fa0b822f6924ace717d4
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-10-01 22:08:28 +0200

    Feat: FInish 09_palindromes challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const normalString = str.toLowerCase()
+                                  .split('')
+                                  .join('')
+                                  .replace(/[^a-z0-9]+/g, '');
+  const reversedString = str.toLowerCase()
+                                  .split('')
+                                  .reverse()
+                                  .join('')
+                                  .replace(/[^a-z0-9]+/g, '');
+  return normalString === reversedString ? true : false;
 };
 
 // Do not edit below this line
