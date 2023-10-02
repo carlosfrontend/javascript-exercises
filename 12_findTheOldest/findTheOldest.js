6b772f1d8cf328af00b660a579c709cb9ec2ca98
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-10-02 21:33:54 +0200

    Feat: Finish 12_findTheOldest challenge

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (persons) {
+
+  const died = persons.some(person => isNaN(person.age) ? true : false);
+
+  const addAge = persons.map(person => {  
+    person.age = person.yearOfDeath - person.yearOfBirth;
+  });
+
+  const orderedPersons = persons.sort((personOld, personYoung) => {
+    if(personYoung.yearOfDeath === undefined){
+      personYoung.age = new Date().getFullYear() - personYoung.yearOfBirth;
+    }
+    if(died){
+      return personOld.age > personYoung.age ? -1 : 1;
+    }
+  });
+  // Carly is immortal!
+  const oldest = orderedPersons[0];
+  console.log(oldest)
+  return oldest;
 
 };
 
